c1b96763a18f0662f1cc2e3cd581357a1fafa1bf
Akash Agrahari <akash.agrahari@gmail.com>
2017-07-06 20:02:24 +0530

    Adding shade plugin

--- core/src/test/java/com/example/question/BlogJDBIDataSourceTest.java
+++ core/src/test/java/com/example/question/BlogJDBIDataSourceTest.java
@@ -9,25 +9,19 @@ import com.google.common.collect.Lists;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
 
 import java.sql.Timestamp;
-import java.util.UUID;
 
 import static org.mockito.Mockito.when;
 
 /**
  * Created by akash.a on 04/07/17.
  */
-@PrepareForTest({ UUID.class })
-@RunWith(PowerMockRunner.class)
 public class BlogJDBIDataSourceTest {
 
     private BlogJDBIDataSource datasource;
